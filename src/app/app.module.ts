80caac570df522fec8e13380fadc92fb4b7f7a3b
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-22 11:13:14 +0530

    Removed body and footer component from app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 import { HeaderNavigationComponent } from './header/header-navigation/header-navigation.component';
 import { HeaderContentComponent } from './header/header-content/header-content.component';
 
@@ -20,8 +18,6 @@ import {MatIconModule} from '@angular/material/icon';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    BodyComponent,
     HeaderNavigationComponent,
     HeaderContentComponent
   ],
